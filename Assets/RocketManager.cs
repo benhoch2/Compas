24ad257d188a2fa08202164ade6f11b0a8afe35d
benhoch2 <benhoch2@gmail.com>
2020-05-02 23:11:24 +0300

    rockets works

--- Assets/RocketManager.cs
+++ Assets/RocketManager.cs
@@ -8,32 +8,67 @@ public class RocketManager : MonoBehaviour
     [SerializeField] GameObject leftRocket;
     [SerializeField] GameObject rightRocket;
     [SerializeField] OvrAvatar localAvatar;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float thrustFactor;
+
+    bool leftIndex, rightIndex, leftHand, rightHand;
+    float leftThrust, rightThrust;
 
     bool rocketsEnabled = false;
-    void Start()
-    {
-        
-    }
+    bool canToggle = true;
+
 
     // Update is called once per frame
     void Update()
     {
-        bool leftIndex = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
-        bool rightIndex = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
-        bool leftHand = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch);
-        bool rightHand = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+        GetInput();
+        ToggleRockets();
+    }
 
-        Debug.Log(leftIndex + ", " + rightIndex + ", " + leftHand + ", " + rightHand);
-        //if (leftIndex && rightIndex && leftHand && rightHand)
-        if (leftIndex)
+    private void FixedUpdate()
+    {
+        ApplyRocketForce();
+    }
+
+    private void ApplyRocketForce()
+    {
+        //add force
+        //rigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
+        rb.AddForce(leftRocket.transform.forward * leftThrust * thrustFactor);
+        rb.AddForce(leftRocket.transform.forward * rightThrust * thrustFactor);
+    }
+
+    private void ToggleRockets()
+    {
+        if (canToggle && leftIndex && rightIndex && leftHand && rightHand)
         {
             Debug.Log("toggling rockets");
             rocketsEnabled = !rocketsEnabled;
+            canToggle = false;
+            StartCoroutine(UnlockRockets());
         }
 
         leftRocket.SetActive(rocketsEnabled);
         rightRocket.SetActive(rocketsEnabled);
-        //localAvatar.enabled = !rocketsEnabled;
+        //localAvatar.SetActive(!rocketsEnabled);
     }
 
+    private void GetInput()
+    {
+        leftIndex = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+        rightIndex = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
+        leftHand = OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        rightHand = OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+
+        leftThrust = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+        rightThrust = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
+    }
+
+    private IEnumerator UnlockRockets()
+    {
+        yield return new WaitForSeconds(1f);
+        canToggle = true;
+        Debug.Log("unlock rockets");
+
+    }
 }
